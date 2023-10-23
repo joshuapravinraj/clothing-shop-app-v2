36c5a0ebe758132b7a76eec7c63511d07dc2d914
Joshua Raj <joshua.raj@kodebaze.com>
2023-10-23 23:22:16 +0800

    type script updates

---
+++ src/store/categories/categories.action.ts
@@ -0,0 +1,52 @@
+// Import Types
+import { CATEGORIES_ACTION_TYPES, Category } from "./categories.types";
+
+// Import Utils
+import {
+    createAction,
+    Action,
+    ActionWithPayload,
+} from "../../utils/reducer/reducer.utils";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
+export type FetchCategoriesStart =
+    Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
+
+export type FetchCategoriesSuccess = ActionWithPayload<
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    Category[]
+>;
+
+export type FetchCategoriesFailed = ActionWithPayload<
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+    Error
+>;
+
+export type CategoryAction =
+    | FetchCategoriesStart
+    | FetchCategoriesSuccess
+    | FetchCategoriesFailed;
+
+export const fetchCategoriesStart = (): FetchCategoriesStart =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (
+    categoriesArray: Category[]
+): FetchCategoriesSuccess =>
+    createAction(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+        categoriesArray
+    );
+
+export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+        const categoriesArray = await getCategoriesAndDocuments();
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+        dispatch(fetchCategoriesFailed(error));
+    }
+};
